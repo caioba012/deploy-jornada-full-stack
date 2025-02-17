86c36a30307a3d199a6c07e6304ec86ba42b2e70
Caio Chiqueto <chiqueto.caio@gmail.com>
2025-02-16 23:56:13 -0300

    ajustes para o deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,27 +1,32 @@
 import express, { request, response } from "express";
 import cors from "cors";
 import { db } from "../api/connect.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3001;
 
-app.use(cors({
-    origin: 'http://localhost:5173', // Permitir apenas o frontend local
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  }));
+app.use(cors());
 
-app.get("/", (request, response) => {
+app.get("/api/", (request, response) => {
     response.send("Hello World!");
 });
 
-app.get("/artists", async (request, response) => {
+app.get("/api/artists", async (request, response) => {
     response.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
     response.send(await db.collection("songs").find({}).toArray());
 });
+
+app.use(express.static(path.join(__dirname, "../../front-end/dist")));
+
+app.get("*", async (request, response) => {
+    response.sendfile(path.join(__dirname, "../../front-end/dist/dist/index.html"));
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor está escutando a porta ${PORT}`);
