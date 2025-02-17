fda9ba5a39fc2df764cfc7893eba0e32abd57825
Caio Chiqueto <chiqueto.caio@gmail.com>
2025-02-17 00:08:35 -0300

    update route

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -22,10 +22,10 @@ app.get("/api/songs", async (request, response) => {
     response.send(await db.collection("songs").find({}).toArray());
 });
 
-app.use(express.static(path.join(__dirname, "../../front-end/dist")));
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
 
 app.get("*", async (request, response) => {
-    response.sendfile(path.join(__dirname, "../../front-end/dist/index.html"));
+    response.sendfile(path.join(__dirname, "../front-end/dist/index.html"));
 })
 
 app.listen(PORT, () => {
